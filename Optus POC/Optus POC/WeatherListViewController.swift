06dcf5882b76fb96ff363319a4986f1558f6de42
Rohit Shivankar <rs00332862@techmahindra.com>
2020-05-21 15:35:52 +0530

    Created Single view applciation for Optus POC Deployment target is set to iOS 12

---
+++ Optus POC/Optus POC/WeatherListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherListViewController.swift
+//  Optus POC
+//
+//  Created by Rohit on 5/21/20.
+//  Copyright © 2020 Rohit. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
